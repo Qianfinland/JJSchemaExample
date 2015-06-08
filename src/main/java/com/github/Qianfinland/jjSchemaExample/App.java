791c5a8053736ae288b0cfbe996c2e3f61c97244
Qian Zhou <qian.zhou@npsopt.com>
2015-06-08 14:43:18 +0300

    first commit

---
+++ src/main/java/com/github/Qianfinland/jjSchemaExample/App.java
@@ -0,0 +1,13 @@
+package com.github.Qianfinland.jjSchemaExample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
