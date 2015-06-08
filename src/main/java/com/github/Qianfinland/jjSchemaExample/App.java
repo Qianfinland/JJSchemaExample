fa0e5f1a472d0bb523e8280b236913f9bf33c0d0
Qian Zhou <qianfinland@gmail.com>
2015-06-08 15:57:54 +0300

    create a txt file and save the console output to it

--- src/main/java/com/github/Qianfinland/jjSchemaExample/App.java
+++ src/main/java/com/github/Qianfinland/jjSchemaExample/App.java
@@ -1,8 +1,12 @@
 package com.github.Qianfinland.jjSchemaExample;
 
+
+
 import com.fasterxml.jackson.databind.JsonNode;
 import com.github.reinert.jjschema.JsonSchemaGenerator;
 import com.github.reinert.jjschema.SchemaGeneratorBuilder;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 /**
  * Hello world!
@@ -16,5 +20,17 @@ public class App
         JsonSchemaGenerator v4generator = SchemaGeneratorBuilder.draftV4Schema().build();
         JsonNode productSchema = v4generator.generateSchema(Product.class);
         System.out.println(productSchema);
+        //save the schema file
+        System.out.println("Schema file from Java Class is created successfully!");
+        
+        try {
+			PrintWriter writer = new PrintWriter("schema.txt");
+			writer.println(productSchema);
+			writer.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+      
     }
 }
